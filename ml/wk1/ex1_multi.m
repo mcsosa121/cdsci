b8c8eb442eb8b84d2a1752d712317bd1d59e2a0e
Mike Sosa <mcsosa121@gmail.com>
2018-04-05 02:00:06 -0400

    Finish week 2

--- ml/wk1/ex1_multi.m
+++ ml/wk1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.1;
+num_iters = 100;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,7 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+p1 = (1650 - mu(1))/ sigma(1);
+p2 = (3 - mu(2))/ sigma(2);
+price = theta'*[1;p1;p2]; 
 
 
 % ============================================================
@@ -149,7 +151,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = theta'*[1;1650;3]; % You should change this
 
 
 % ============================================================
--- ml/wk1/normalEqn.m
+++ ml/wk1/normalEqn.m
@@ -1,23 +1,10 @@
 function [theta] = normalEqn(X, y)
-%NORMALEQN Computes the closed-form solution to linear regression 
-%   NORMALEQN(X,y) computes the closed-form solution to linear 
-%   regression using the normal equations.
+    %NORMALEQN Computes the closed-form solution to linear regression 
+    %   NORMALEQN(X,y) computes the closed-form solution to linear 
+    %   regression using the normal equations.
 
-theta = zeros(size(X, 2), 1);
+    theta = zeros(size(X, 2), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the code to compute the closed form solution
-%               to linear regression and put the result in theta.
-%
-
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-% -------------------------------------------------------------
-
-
-% ============================================================
-
+    t = X'*y;
+    theta = (X'*X) \ t;
 end
